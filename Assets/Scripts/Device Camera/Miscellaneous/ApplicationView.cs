365efbd6688023c109de27712e00e45e6dbbfc6c
Panagiotis Migkotzidis <panagiotismigo@gmail.com>
2019-07-12 17:45:46 +0300

    Tiny Yolo added

--- Assets/Scripts/Device Camera/Miscellaneous/ApplicationView.cs
+++ Assets/Scripts/Device Camera/Miscellaneous/ApplicationView.cs
@@ -3,32 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ApplicationView : MonoBehaviour
+namespace EVISION.Camera.plugin
 {
-    public GameObject image;
-    public Text text;
-    public ClientApplication clientApp;
-    public Text deviceNumberText;
 
-    // Start is called before the first frame update
-    void Start()
+    public class ApplicationView : MonoBehaviour
     {
-        image.SetActive(false);
-    }
+        public GameObject image;
+        public Text text;
+        public ClientApplication clientApp;
+        public Text deviceNumberText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(clientApp.annotationProccessBusy)
-        {
-            image.SetActive(true);
-            //Debug.Log("Annotating");
-        }
-        else if (!clientApp.annotationProccessBusy)
+        // Start is called before the first frame update
+        void Start()
         {
             image.SetActive(false);
         }
 
-        deviceNumberText.text = WebCamTexture.devices.Length.ToString();
+        // Update is called once per frame
+        void Update()
+        {
+            if (clientApp.annotationProccessBusy)
+            {
+                image.SetActive(true);
+                //Debug.Log("Annotating");
+            }
+            else if (!clientApp.annotationProccessBusy)
+            {
+                image.SetActive(false);
+            }
+
+            deviceNumberText.text = WebCamTexture.devices.Length.ToString();
+        }
     }
 }
--- Assets/Scripts/Device Camera/Miscellaneous/Utils.cs
+++ Assets/Scripts/Device Camera/Miscellaneous/Utils.cs
@@ -4,43 +4,46 @@ using System;
 using UnityEngine;
 using System.Linq;
 
-public static class Utils
+namespace EVISION.Camera.plugin
 {
-    public static IEnumerator PerformMajorityVoting(List<string> OCR_List)
+    public static class Utils
     {
-        MajorityVoting maj = new MajorityVoting();
-        string product = maj.PerformMajorityVoting(OCR_List);
-        yield return null;
-    }
-
-    public static List<string> SplitStringToList(string text)
-    {
-        List<string> l = text.Split(new Char[] { ' ', '\n' }).ToList();
-        return l;
-    }
-
-    public static Texture2D RotateTexture(Texture2D originalTexture, bool clockwise)
-    {
-        Color32[] original = originalTexture.GetPixels32();
-        Color32[] rotated = new Color32[original.Length];
-        int w = originalTexture.width;
-        int h = originalTexture.height;
-
-        int iRotated, iOriginal;
-
-        for (int j = 0; j < h; ++j)
+        public static IEnumerator PerformMajorityVoting(List<string> OCR_List)
         {
-            for (int i = 0; i < w; ++i)
-            {
-                iRotated = (i + 1) * h - j - 1;
-                iOriginal = clockwise ? original.Length - 1 - (j * w + i) : j * w + i;
-                rotated[iRotated] = original[iOriginal];
-            }
+            MajorityVoting maj = new MajorityVoting();
+            string product = maj.PerformMajorityVoting(OCR_List);
+            yield return null;
         }
 
-        Texture2D rotatedTexture = new Texture2D(h, w);
-        rotatedTexture.SetPixels32(rotated);
-        rotatedTexture.Apply();
-        return rotatedTexture;
+        public static List<string> SplitStringToList(string text)
+        {
+            List<string> l = text.Split(new Char[] { ' ', '\n' }).ToList();
+            return l;
+        }
+
+        public static Texture2D RotateTexture(Texture2D originalTexture, bool clockwise)
+        {
+            Color32[] original = originalTexture.GetPixels32();
+            Color32[] rotated = new Color32[original.Length];
+            int w = originalTexture.width;
+            int h = originalTexture.height;
+
+            int iRotated, iOriginal;
+
+            for (int j = 0; j < h; ++j)
+            {
+                for (int i = 0; i < w; ++i)
+                {
+                    iRotated = (i + 1) * h - j - 1;
+                    iOriginal = clockwise ? original.Length - 1 - (j * w + i) : j * w + i;
+                    rotated[iRotated] = original[iOriginal];
+                }
+            }
+
+            Texture2D rotatedTexture = new Texture2D(h, w);
+            rotatedTexture.SetPixels32(rotated);
+            rotatedTexture.Apply();
+            return rotatedTexture;
+        }
     }
 }
