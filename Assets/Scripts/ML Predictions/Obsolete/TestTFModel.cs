7afbe18141a84d300f48c4404d4b5aa61c788d45
Panagiotis Migkotzidis <panagiotismigo@gmail.com>
2019-12-18 13:36:48 +0200

    Object detection

--- Assets/Scripts/ML Predictions/Obsolete/TestTFModel.cs
+++ Assets/Scripts/ML Predictions/Obsolete/TestTFModel.cs
@@ -12,32 +12,27 @@ public class TestTFModel : MonoBehaviour
     public string image_file;
     public int angle;
     private IModelPrediction classifier_2;
+    private IModelPrediction object_detector;
     // Start is called before the first frame update
     void Start()
     {
 
-        TFSharpClassification classifier = new TFSharpClassification("input_1", "Logits/Softmax", 224, 224, 127.5f, 127.5f, model, labelsFile, angle, 0.05f);
+        //TFSharpClassification classifier = new TFSharpClassification("input_1", "Logits/Softmax", 224, 224, 127.5f, 127.5f, model, labelsFile, angle, 0.05f);
 
-        var tex = Resources.Load<Texture2D>("Textures/Masoutis/" + image_file);
+        var tex = Resources.Load<Texture2D>("Products_UnitTests/" + image_file);
         //var tex = Resources.Load<Texture2D>("Textures/" + image_file);
 
         //Texture2D tex = new Texture2D(img.texture.width, img.texture.height);
         //tex = img.texture as Texture2D;
 
-        RawImage img2 = GameObject.Find("img2").GetComponent<RawImage>();
-        img2.texture = tex;;
+        Detector detector = new Detector(model, labelsFile, DetectionModels.YOLO);
+        var x = detector.Detect(tex);
+        Debug.Log(x[0]);
 
-        //var oute = classifier.FetchOutput(tex);
-        //foreach (KeyValuePair<string, float> value in oute)
-        //{
-        //    Debug.Log("class :" + value.Key + ": " + value.Value);
-        //}
+        //TFDetection detector = new TFDetection("input", "output", 224, 224, 127.5f, 127.5f, model, labelsFile, angle, 0.05f);
 
 
-
-        //print("===============================");
-
-        //IModelPrediction classifier_3 = new TFClassification("input_1", "Logits/Softmax", 224, 224, 127.5f, 127.5f, model, labelsFile, angle, 0.01f);
+        //IModelPrediction classifier_3 = new TFDetection("input", "output", 224, 224, 127.5f, 127.5f, model, labelsFile, angle, 0.05f);
 
         //var output = classifier_3.FetchOutput<IList, Texture2D>(tex);
         //foreach (KeyValuePair<string, float> value in output)
@@ -45,20 +40,21 @@ public class TestTFModel : MonoBehaviour
         //    Debug.Log("class :" + value.Key + ": " + value.Value);
         //}
 
-        IModelPrediction classifier_4 = new TFFeatureExtraction("input_1", "block_15_project/convolution", 224, 224, 127.5f, 127.5f, model, labelsFile, angle, 0.01f);
-        var output = classifier_4.FetchOutput<List<float>, Texture2D>(tex);
+        // With SVM and deep Network.
+        //IModelPrediction classifier_4 = new TFFeatureExtraction("input_1", "block_15_project/convolution", 224, 224, 127.5f, 127.5f, model, labelsFile, angle, 0.01f);
+        //var output = classifier_4.FetchOutput<List<float>, Texture2D>(tex);
 
-        double[] output_array = GenericUtils.ConvertToDouble(output.ToArray());
+        //double[] output_array = GenericUtils.ConvertToDouble(output.ToArray());
 
-        SVMClassification obj = new SVMClassification();
-        obj.SetModelParameters("SVM_Weights", "mu", "sigma");
-        var norm_fv = obj.NormalizeElements(output_array, obj.muData, obj.sigmaData);
-        List<double> norm_fv_list = new List<double>(norm_fv);
-        IModelPrediction svm = obj;
-        var probs = svm.FetchOutput<List<float>, List<double>>(norm_fv_list);
-        foreach (var item in probs)
-        {
-            Debug.Log(item);
-        }
+        //SVMClassification obj = new SVMClassification();
+        //obj.SetModelParameters("SVM_Weights", "mu", "sigma");
+        //var norm_fv = obj.NormalizeElements(output_array, obj.muData, obj.sigmaData);
+        //List<double> norm_fv_list = new List<double>(norm_fv);
+        //IModelPrediction svm = obj;
+        //var probs = svm.FetchOutput<List<float>, List<double>>(norm_fv_list);
+        //foreach (var item in probs)
+        //{
+        //    Debug.Log(item);
+        //}
     }
 }
---
+++ Assets/Scripts/ML Predictions/TFDetection.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using TensorFlow;
+
+/// <summary>
+/// This class implements the IModelPrediction interface
+/// and aims to extract features from a pre-trained tensorflow
+/// model. To fetch output the input is a texture2D and the output
+/// is an IList (IList< string , float >).
+/// </summary>
+
+public class TFDetection : IModelPrediction
+{
+    private string inputName;
+    private string outputName; 
+    private int inputHeight;
+    private int inputWidth;
+    private float inputMean;
+    private float inputStd;
+    private TextAsset modelFile; 
+    private TextAsset labelFile; 
+    private int angle;
+    private float thresshold = 0.05f;
+    private string[] labels;
+    private int numOfResults = 5;
+
+    private TFGraph graph;
+    private TFSession session;
+
+    // Constructor of the class
+    public TFDetection(string inputName, string outputName, int inputHeight, int inputWidth, float inputMean,
+        float inputStd, TextAsset modelFile, TextAsset labelFile, int angle, float thresshold)
+    {
+        this.inputName = inputName;
+        this.outputName = outputName;
+        this.inputHeight = inputHeight;
+        this.inputWidth = inputWidth;
+        this.inputMean = inputMean;
+        this.inputStd = inputStd;
+        this.modelFile = modelFile;
+        this.labelFile = labelFile;
+        this.angle = angle;
+        this.thresshold = thresshold;
+
+        graph = new TFGraph();
+        graph.Import(modelFile.bytes);
+        session = new TFSession(graph);
+        labels = labelFile.text.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    /// <summary>
+    /// This is the model inference method. You specify an input and the relevant output
+    /// of the model is extracted.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="inputTex"></param>
+    /// <returns></returns>
+    public T FetchOutput<T,U>(U inputTex) where T: class, IList where U : class
+    {
+        //////// TODO : shape should be passed as argument to Transform Input
+        var shape = new TFShape(1, inputWidth, inputHeight, 3);
+        ////////
+        
+        var scaled = TextureTools.scaled(inputTex as Texture2D, 224, 224, FilterMode.Trilinear);
+        var rotated = TextureTools.RotateImageMatrix(scaled.GetPixels32(), scaled.width, scaled.height, 180);
+
+        var input = graph[inputName][0];
+
+        var inputTensor = TFSharpUtils.TransformInput(rotated, 224, 224, inputMean, inputStd);
+
+        var runner = session.GetRunner();
+        runner.AddInput(input, inputTensor).Fetch(graph[outputName][0]);
+
+        var output = runner.Run()[0];
+        var outputs = output.GetValue() as float[,];
+
+        inputTensor.Dispose();
+        output.Dispose();
+
+        var list = new List<KeyValuePair<string, float>>();
+
+        for (int i = 0; i < labels.Length; i++)
+        {
+            var confidence = outputs[0, i];
+            if (confidence < 0.01f) continue;
+
+            list.Add(new KeyValuePair<string, float>(labels[i], confidence));
+        }
+
+        var results = list.OrderByDescending(i => i.Value).Take(numOfResults).ToList();
+
+        return results as T;
+    }
+}
