5a7c4a59153c8e37c58534cebb8068e04bb0c75c
Panagiotis Migkotzidis <panagiotismigo@gmail.com>
2020-01-15 17:45:56 +0200

    Documentation commited

--- Assets/Scripts/CameraClient.cs
+++ Assets/Scripts/CameraClient.cs
@@ -24,14 +24,19 @@ namespace EVISION.Camera.plugin
 
 
         #region Event Listeners
-
+        /// <summary>  
+        /// Event Listener που διαχειρίζεται την αποτυχία επικοινωνίας με το τρέχον annotation service
+        /// του πεδίου της διεπαφής IAnnotate.  
+        /// </summary>  
         public void AnnotationFailedHandler()
         {
             StopAllCoroutines();
             StartCoroutine(voiceSynthesizer.PerformSpeechFromText("Η σύνδεση στο δίκτυο είναι απενεργοποιημένη. Παρακαλώ, ενεργοποιήστε την σύνδεση στο διαδύκτιο."));
             annotationProccessBusy = false;
         }
-
+        /// <summary>  
+        /// Μέθοδος που αναλαμβάνει την ακύρωση όλων των κορουτινών και μεθόδων που είναι ενεργές.  
+        /// </summary>  
         public virtual void CancelButton()
         {
             if (!annotationProccessBusy)
@@ -43,7 +48,10 @@ namespace EVISION.Camera.plugin
             camTexture = null;
             annotationProccessBusy = false;
         }
-
+        /// <summary>  
+        /// Μέθοδος που αναλαμβάνει την ενεργοποίηση και σύνδεση της ενσωματωμένης κάμερας της συσκευής.
+        /// Επίσης απενεργοποιεί όλες τις διεπάφές που χρησιμοποιούνται από την κλάση του πεδίου httpLoader.
+        /// </summary>  
         public void ConnectNativeCamera()
         {
             voiceSynthesizer.PerformSpeechFromText("Εξωτερική κάμερα απενεργοποιήθηκε... κάμερα κινητού ενεργοποιημένη");
@@ -54,7 +62,12 @@ namespace EVISION.Camera.plugin
 
         #endregion
 
-
+        /// <summary>  
+        /// Μέθοδος που αναλαμβάνει την λήψη στιγμιοτύπου από την τρέχουσα ενεργοποιημένη κάμερα. Εάν η κάμερα είναι η ενσωματωμένη
+        /// της συσκευής η διεπαφή IDeviceCamera χρησιμοπιείται. Αλλιώς γίνετε χρήση του αντικειμένου της κλάσης HttpImageLoading 
+        /// και γίνετε λήψη από την προς χρήση εξωτερική κάμερα.  
+        /// </summary>   
+        /// <returns>Αντικείμενο τύπου IEnumerator</returns>
         protected IEnumerator GetScreenshot()
         {
             float startCapture = Time.realtimeSinceStartup;
@@ -96,9 +109,20 @@ namespace EVISION.Camera.plugin
             float endCapture = Time.realtimeSinceStartup;
             captureTime = GenericUtils.CalculateTimeDifference(startCapture, endCapture);
         }
-
+        /// <summary>  
+        /// Κορουτίνα που αναλαμβάνει τις απαραίτητες κλήσεις στις αντίστοιχες μεθόδους, με σκοπό την λήψη
+        /// στιγμιοτύπου, κατηγοριοποίηση εικόνας και αποστολή στο προς χρήση annotation service (e.g google cloud vision). 
+        /// </summary>  
+        /// <returns>Αντικείμενο τύπου IEnumerator</returns>
         public abstract IEnumerator ProcessScreenshotAsync();
+        /// <summary>  
+        /// Μέθοδος που αποθηκεύει το στιγμιότυπο της κάμερας. Η διαδικασία της αποθήκευσης γίνετε μέσω της 
+        /// υλοποιημένης μεθόδου της κλάσης που χρησιμοποιεί την διεπαφή IDeviceCamera.
+        /// </summary>  
+        /// <param name="camTexture">Αντικείμενο τύπου Texture2D</param>  
+        /// <returns>Αντικείμενο τύπου IEnumerator</returns>
         public abstract void SaveScreenshot(Texture2D camTexture);
+
         public abstract void SetResultLogs();
     }
 
